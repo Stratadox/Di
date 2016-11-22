f0e1ec27efdf292448e0ead6d70b145c2b6d7e1e
Stratadox <jesse@stratadox.com>
2016-11-22 01:21:59 +0100

    Allow the cache to be turned off

--- src/ContainerInterface.php
+++ src/ContainerInterface.php
@@ -31,7 +31,7 @@ use Closure;
 
 interface ContainerInterface
 {
-    public function set($name, Closure $factory);
+    public function set($name, Closure $factory, $cache = false);
     public function get($name, $type = '');
     public function has($name);
     public function forget($name);
