7ca8d35c89a7fb9e77f6e48d70407509274cb87e
Stratadox <jesse@stratadox.com>
2018-02-06 23:28:21 +0100

    Add assertion for exception message about autowired sclars

--- tests/ScalarsCannotBeAutoWiredTest.php
+++ tests/ScalarsCannotBeAutoWiredTest.php
@@ -19,9 +19,13 @@ class ScalarsCannotBeAutoWiredTest extends TestCase
     /** @test */
     function throwing_an_exception_when_trying_to_autowire_a_class_with_scalar_dependencies()
     {
-        $di = AutoWiring::the(new Container);
+        $container = AutoWiring::the(new Container);
 
         $this->expectException(ScalarsCannotBeAutoWired::class);
-        $di->get(Qux::class);
+        $this->expectExceptionMessage(
+            'Cannot autowire the string argument.'
+        );
+
+        $container->get(Qux::class);
     }
 }
