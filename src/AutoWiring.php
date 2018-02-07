845ef363f95a50caaadc6a27e52c13cbfd591ce0
Stratadox <jesse@stratadox.com>
2018-02-07 01:02:54 +0100

    Have AutoWiring implement and delegate set and forget

--- src/AutoWiring.php
+++ src/AutoWiring.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 
 namespace Stratadox\Di;
 
+use Closure;
 use Psr\Container\ContainerInterface as PsrContainerInterface;
 use ReflectionClass as Reflected;
 use ReflectionException;
 use ReflectionType;
 
-final class AutoWiring implements PsrContainerInterface
+final class AutoWiring implements ContainerInterface, PsrContainerInterface
 {
     private $container;
     private $links;
@@ -30,7 +31,7 @@ final class AutoWiring implements PsrContainerInterface
         return new self($this->container, [$interface => $class] + $this->links);
     }
 
-    public function get($theService)
+    public function get($theService, string $type = '')
     {
         if (!$this->container->has($theService)) {
             $this->resolve($theService);
@@ -43,6 +44,16 @@ final class AutoWiring implements PsrContainerInterface
         return class_exists($theService)
             || isset($this->links[$theService])
             || $this->container->has($theService);
+    }
+
+    public function set(string $service, Closure $factory, bool $cache = true)
+    {
+        $this->container->set($service, $factory, $cache);
+    }
+
+    public function forget(string $service)
+    {
+        $this->container->forget($service);
     }
 
     private function resolve(string $service)
